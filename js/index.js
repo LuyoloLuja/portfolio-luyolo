788b0f2dde00dcc94e52cc8e68941f5f5cc2a7b9
LuyoloLuja <luyolom19@gmail.com>
2021-01-29 19:58:20 +0200

    made nav responsive

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
-const harmburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const myImage = document.querySelector('.my-image');
 
-function removeImageInMobile(screenSize){
-    if(screenSize.matches){
-        myImage.parentNode.removeChild(myImage);
-    }
+// adds animations
+function toggleAnimation() {
+    navLinks.classList.toggle("open");
+
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    })
 }
 
-let screen = window.matchMedia("(max-width: 768px)");
-removeImageInMobile(screen);
-screen.addEventListener(removeImageInMobile);
+// adds style for nav after hamburger is clicked
+function addStyleOnClick(){
+        
+        // navLinks.style.backgroundColor = "#5D5C6199";
+        // navLinks.style.color = "#313036";
 
-// harmburger.addEventListener('click', () => {
-//     navLinks.classList.toggle("open");
 
-//     links.forEach(link => {
-//         link.classList.toggle("fade");
-//     });
-// })
+        // myImage.style.opacity = "0.5";
+        // if (hamburger === true) {
+        //     navLinks.style.backgroundColor = "white";
+        //     navLinks.style.opacity = "0.5";
+        // } else {
+        //     navLinks.parentNode.removeChild(navLinks);
+        // }
+}
+
+hamburger.addEventListener("click", () => {
+    toggleAnimation();
+    addStyleOnClick();
+})
+
+// function removeImageInMobile(screenSize){
+//     if(screenSize.matches){
+//         myImage.parentNode.removeChild(myImage);
+//     }
+// }
+
+// let screen = window.matchMedia("(max-width: 768px)");
+// removeImageInMobile(screen);
+// screen.addEventListener(removeImageInMobile);
