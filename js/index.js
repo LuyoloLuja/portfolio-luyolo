288d732b274536050bd5bc470cf15c21953762a2
LuyoloLuja <luyolom19@gmail.com>
2021-01-29 14:00:51 +0200

    made portfolio responsive

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 const harmburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const myImage = document.querySelector('.my-image');
 
-harmburger.addEventListener('click', () => {
-    navLinks.classList.toggle("open");
+function removeImageInMobile(screenSize){
+    if(screenSize.matches){
+        myImage.parentNode.removeChild(myImage);
+    }
+}
 
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
-})
+let screen = window.matchMedia("(max-width: 768px)");
+removeImageInMobile(screen);
+screen.addEventListener(removeImageInMobile);
+
+// harmburger.addEventListener('click', () => {
+//     navLinks.classList.toggle("open");
+
+//     links.forEach(link => {
+//         link.classList.toggle("fade");
+//     });
+// })
