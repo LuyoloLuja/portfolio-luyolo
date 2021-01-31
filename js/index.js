366f6b36a43d3a9954b6c704e49d5cb91770295c
LuyoloLuja <luyolom19@gmail.com>
2021-01-31 12:38:45 +0200

    changed my color scheme

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const myImage = document.querySelector('.my-image');
+const projects = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
 
 // got to top button
 const scrollUpBtn = document.querySelector(".scrollUpBtn");
@@ -40,12 +42,15 @@ hamburger.addEventListener("click", () => {
     // addStyleOnClick();
 })
 
-// function removeImageInMobile(screenSize){
+// function changeBgColors(screenSize){
 //     if(screenSize.matches){
-//         myImage.parentNode.removeChild(myImage);
+//         projects.style.backgroundColor = "#17b978";
+//         projects.style.color = "#086972";
+
+//         contact.style.backgroundColor = "#a7ff83";
+//         contact.style.color = "#086972";
 //     }
 // }
-
 // let screen = window.matchMedia("(max-width: 768px)");
-// removeImageInMobile(screen);
-// screen.addEventListener(removeImageInMobile);
+// changeBgColors(screen);
+// screen.addEventListener(changeBgColors);
